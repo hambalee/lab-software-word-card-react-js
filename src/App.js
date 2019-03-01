231578662352a14236deb63d5534fcc188b8380e
hambalee <hambalee@outlook.com>
2019-03-01 19:48:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import CharacterCard from './CharacterCard';
 import './App.css';
+import WordCard from './WordCard';
+
+//const word = "Hello"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+ render() {
+ return (
+ <div className="App">
+  {/*
+    Hello World
+   <Component2 /> 
+  <div>
+      <CharacterCard value="h"/>
+      <CharacterCard value="i"/>
+  </div>
+  */}
+ 
+ <div>
+ {/*
+ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>)
+ */}
+ <WordCard value="hello"/>
+ </div>
 
+ </div>
+ 
+
+ );
+ }
+}
 export default App;
